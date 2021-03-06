3fb59ec6adf307c470f4c00818cbfb1df8add3a5
jaeeunhyun <rosa2070@naver.com>
2021-03-06 23:58:20 +0900

    dfsdgags

--- BBS/src/bbs/BbsDAO.java
+++ BBS/src/bbs/BbsDAO.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
+
 public class BbsDAO {
 	
 	private Connection conn;
@@ -129,8 +131,35 @@ public class BbsDAO {
 		}
 		return null;
 	}
+		
+	public int update(int bbsID, String bbsTitle, String bbsContent) {
+		String SQL = "UPDATE BBS SET bbsTitle = ?, bbsContent = ? WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, bbsTitle);
+			pstmt.setString(2, bbsContent);
+			pstmt.setInt(3, bbsID);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+		
+	}
+
+	public int delete(int bbsID) {
+		String SQL = "UPDATE BBS SET bbsAvailable = 0 WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;//데이터베이스 오류
+	}
 	
-	
+}
 	
 	
 	
@@ -156,4 +185,4 @@ public class BbsDAO {
 	
 	
 
-}
+
